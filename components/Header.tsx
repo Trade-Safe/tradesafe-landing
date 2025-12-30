7b739e60f9ef2abaec194dcadc341f58de4154a6
Trade-Safe <leo.coquard@gmail.com>
2025-12-30 05:30:02 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -83,7 +83,6 @@ export default function Header() {
           transform transition-transform duration-300 ease-in-out md:hidden
           ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}
         `}>
-        `}>
           <div className="flex flex-col h-full pt-20 px-6">
             {/* Mobile Navigation Links */}
             <nav className="flex flex-col gap-6 mb-8">
