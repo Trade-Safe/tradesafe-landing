cd93608f6392856380510851e20f8b747a4c1c1d
Trade-Safe <leo.coquard@gmail.com>
2026-01-03 09:14:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
         />
         <Pricing {...messages.pricing} />
         <HowItWorks {...messages.howItWorks} />
-        <WhatWeAreNot {...messages.whatWeAreNot} />
       </main>
       <Footer {...messages.footer} />
     </>
