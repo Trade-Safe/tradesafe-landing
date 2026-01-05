44b466ea9f1f0eab624c52a529f3e9a83846e0e2
Trade-Safe <leo.coquard@gmail.com>
2026-01-06 00:35:14 +0100

    Update Pricing.tsx

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import EmailFormPricing from './EmailFormPricing'
+import EmailForm from './EmailForm'
 import PricingToggle from './PricingToggle'
 
 interface PricingProps {
